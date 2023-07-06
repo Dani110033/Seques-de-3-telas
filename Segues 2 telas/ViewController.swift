073361456d8dc17787584bc99428b05721746ac5
Enzo <enzo.carvalho.teles@gmail.com>
2023-07-06 18:39:21 -0300

    projetonovo

---
+++ Segues 2 telas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segues 2 telas
+//
+//  Created by Enzo on 06/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
